c2262f3baa208b61d719cc35f9da8973f4dc4d84
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-08-29 20:59:52 +0500

    Create ShowMore component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -7,6 +7,7 @@ import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import CarDetails from "./CarDetails";
 import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
 
 export {
   Hero,
@@ -18,4 +19,5 @@ export {
   CustomFilter,
   CarDetails,
   CarCard,
+  ShowMore
 };
